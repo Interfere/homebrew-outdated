53b0486d83ca523880ffae4038e50986e7ca6b7b
Ahmed Khaled <ahmed.khaled@deliveryhero.com>
2022-06-29 16:30:23 +0200

    change bottle checksums

--- Formula/utica@0.40.0.rb
+++ Formula/utica@0.40.0.rb
@@ -10,9 +10,8 @@ class UticaAT0400 < Formula
 
   bottle do
     root_url "https://github.com/Interfere/Utica/releases/download/0.40.0-pre"
-    sha256 cellar: :any_skip_relocation, arm64_monterey:                          "54d91a0b7c4e8006baa28f26780465e199c5e5afca3111eb24f7078f03dbcf3a"
-    sha256 cellar: :any_skip_relocation, big_sur:                                 "6ab9e7afd303018454de648cabe35c8c3bb184b0f6d415abcb2372ac8445b154"
-    sha256 cellar: :any_skip_relocation, catalina:                                "19bdd1b2c96965cc6cb99baa3cc8e53dec77dd28b08b1f2984828e36fcdf383c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey:                          "dc44b4c000e306907996935e2c8a258b8bf2c85eaeb865c0938e880c1ec51a81"
+    sha256 cellar: :any_skip_relocation, monterey:                                "8c113b2808abd09274f4ce21f880fa185dd054551e174e493fce2d94b495b1b4"
   end
 
   depends_on xcode: ["12.0", :build]
